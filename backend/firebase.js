2d547ee3a2e5a7b7241ca6f2b94fed8a79b1529f
Thomas Brears <thomasbrears123@gmail.com>
2024-10-13 17:17:15 +1300

    Deployment API Issue Fix: added firebase sdk keys

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,10 +1,23 @@
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
+import dotenv from 'dotenv';
 
-const serviceAccount = JSON.parse(readFileSync('./r-and-d-induction-firebase-adminsdk-pf2gf-d8e0622e57.json'));
+dotenv.config();
+
+//const serviceAccount = JSON.parse(readFileSync('./r-and-d-induction-firebase-adminsdk-pf2gf-d8e0622e57.json'));
+
+//admin.initializeApp({
+  //credential: admin.credential.cert(serviceAccount), 
+  //projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,  
+//});
+
+//Firebase service account key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount), 
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,  
 });
 
