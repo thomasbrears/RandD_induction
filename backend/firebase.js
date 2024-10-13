4b1b752e22eda3e4bb9c531cc81e3018b40889b8
Thomas Brears <thomasbrears123@gmail.com>
2024-10-13 17:36:20 +1300

    Deployment view users Issue: adjust firebase keys

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,24 +1,17 @@
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-//const serviceAccount = JSON.parse(readFileSync('./r-and-d-induction-firebase-adminsdk-pf2gf-d8e0622e57.json'));
-
-//admin.initializeApp({
-  //credential: admin.credential.cert(serviceAccount), 
-  //projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,  
-//});
-
-//Firebase service account key
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// Parse the service account JSON from the environment variable
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n') // Replace escaped newlines with actual newlines
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,  
+  projectId: process.env.FIREBASE_PROJECT_ID,  
 });
 
 export const db = admin.firestore();
