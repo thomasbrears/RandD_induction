29e2dae0b2e3cfa0bdb9c9330ff7681ef3657759
Thomas Brears <thomasbrears123@gmail.com>
2024-10-13 17:51:47 +1300

    Deployment view users Issue: adjust firebase keys again

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,16 +2,21 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Parse the service account JSON from the environment variable
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n') // Replace escaped newlines with actual newlines
-);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines with actual newlines
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  projectId: process.env.FIREBASE_PROJECT_ID,  
 });
 
 export const db = admin.firestore();
