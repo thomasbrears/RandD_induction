4d1b44cf292021913185a1516b82c290fe3b7a29
Thomas Brears <thomasbrears123@gmail.com>
2024-10-14 09:28:36 +1300

    Deployment backend api Issue: adjust private keys and db setup

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,21 +2,15 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
+//Firebase service account key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-  }),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`
 });
 
-export const db = admin.firestore();
+const db = admin.firestore();
+
+export { db, admin };
