e2182fcf7aca15600661236a554575777e13b24e
澪 <mio@mio19.uk>
2024-10-14 18:29:41 +1300

    Improve ui on view inductions page

--- frontend/src/components/AssignedInductions.jsx
+++ frontend/src/components/AssignedInductions.jsx
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { getAssignedInductions } from '../api/InductionApi';
 import useAuth from '../hooks/useAuth';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from '@tanstack/react-table';
+import {
+  ArrowUpDown,
+  Search,
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from 'lucide-react';
+import '../style/Table.css'; // Ensure you import your table styles
+
+const columnHelper = createColumnHelper();
 
 const AssignedInductions = ({ uid }) => {
   const [assignedInductions, setAssignedInductions] = useState([]);
   const { user } = useAuth();
+  const [sorting, setSorting] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState('');
 
   // Use uid prop if provided, else default to user.uid
   const userId = uid || user?.uid;
@@ -24,44 +46,204 @@ const AssignedInductions = ({ uid }) => {
     }
   }, [user, userId]);
 
+  const columns = [
+    columnHelper.accessor('name', {
+      cell: (info) => info.getValue(),
+      header: () => (
+        <span className="flex items-center">
+          Induction Name
+        </span>
+      ),
+    }),
+    columnHelper.accessor('availableFrom', {
+      cell: (info) =>
+        info.getValue()
+          ? new Date(info.getValue()).toLocaleDateString()
+          : 'N/A',
+      header: () => (
+        <span className="flex items-center">
+          Available From
+        </span>
+      ),
+    }),
+    columnHelper.accessor('dueDate', {
+      cell: (info) =>
+        info.getValue()
+          ? new Date(info.getValue()).toLocaleDateString()
+          : 'N/A',
+      header: () => (
+        <span className="flex items-center">
+          Due Date
+        </span>
+      ),
+    }),
+    columnHelper.accessor('completionDate', {
+      cell: (info) =>
+        info.getValue()
+          ? new Date(info.getValue()).toLocaleDateString()
+          : 'N/A',
+      header: () => (
+        <span className="flex items-center">
+          Completion Date
+        </span>
+      ),
+    }),
+    columnHelper.accessor('status', {
+      cell: (info) => info.getValue(),
+      header: () => (
+        <span className="flex items-center">
+          Status
+        </span>
+      ),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: assignedInductions,
+    columns,
+    state: {
+      sorting,
+      globalFilter,
+    },
+    initialState: {
+      pagination: {
+        pageSize: 10,
+      },
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
   return (
     <>
-      <div className="flex items-start justify-center pt-8">
-        <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-4xl mx-4 md:mx-8">
-          {assignedInductions.length > 0 ? (
-            <div className="overflow-x-auto">
-              <table className="min-w-full border-collapse border border-gray-200 w-full">
-                <thead>
-                  <tr className="bg-gray-100">
-                    <th className="border border-gray-300 px-4 py-2 text-left">Induction Name</th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">Available From</th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">Due Date</th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">Completion Date</th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {assignedInductions.map((induction, index) => (
-                    <tr key={index}>
-                      <td className="border border-gray-300 px-4 py-2">{induction.name}</td>
-                      <td className="border border-gray-300 px-4 py-2">
-                        {induction.availableFrom ? new Date(induction.availableFrom).toLocaleDateString() : 'N/A'}
-                      </td>
-                      <td className="border border-gray-300 px-4 py-2">
-                        {induction.dueDate ? new Date(induction.dueDate).toLocaleDateString() : 'N/A'}
-                      </td>
-                      <td className="border border-gray-300 px-4 py-2">
-                        {induction.completionDate ? new Date(induction.completionDate).toLocaleDateString() : 'N/A'}
-                      </td>
-                      <td className="border border-gray-300 px-4 py-2">{induction.status}</td>
-                    </tr>
+      <div className="tableContainer">
+        <div className="mb-4 flex items-center gap-4">
+          <div className="relative flex-grow">
+            <input
+              value={globalFilter ?? ''}
+              onChange={(e) => setGlobalFilter(e.target.value)}
+              placeholder="Search..."
+              className="box-size-border w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+            />
+            <Search
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+              size={20}
+            />
+          </div>
+        </div>
+        <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th
+                      key={header.id}
+                      className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none flex items-center'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        <ArrowUpDown className="ml-2" size={14} />
+                      </div>
+                    </th>
                   ))}
-                </tbody>
-              </table>
-            </div>
-          ) : (
-            <p className="text-gray-600">No assigned inductions for this user.</p>
-          )}
+                </tr>
+              ))}
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id} className="hover:bg-gray-50">
+                  {row.getVisibleCells().map((cell) => (
+                    <td
+                      key={cell.id}
+                      className="px-6 py-4 text-sm text-gray-500"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex sm:flex-row justify-between items-center mt-4 text-sm text-gray-700">
+          <div className="flex items-center mb-4 sm:mb-0">
+            <span className="mr-2">Items per page</span>
+            <select
+              className="border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 p-2"
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value));
+              }}
+            >
+              {[10, 20, 30, 40].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex items-center space-x-2">
+            <button
+              className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <ChevronsLeft size={20} />
+            </button>
+            <button
+              className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <ChevronLeft size={20} />
+            </button>
+            <span className="flex items-center">
+              <input
+                min={1}
+                max={table.getPageCount()}
+                type="number"
+                value={table.getState().pagination.pageIndex + 1}
+                onChange={(e) => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                  table.setPageIndex(page);
+                }}
+                className="w-16 p-2 rounded-md border border-gray-300 text-center"
+              />
+              <span className="ml-1">of {table.getPageCount()}</span>
+            </span>
+            <button
+              className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              <ChevronRight size={20} />
+            </button>
+            <button
+              className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+            >
+              <ChevronsRight size={20} />
+            </button>
+          </div>
         </div>
       </div>
     </>
