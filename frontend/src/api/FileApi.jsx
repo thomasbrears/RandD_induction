6cb0af9d0df4b60db4b28a7acd64a53603740653
Thomas Brears <fcx9443@autuni.ac.nz>
2025-06-06 19:05:15 +1200

    dev access

--- frontend/src/api/FileApi.jsx
+++ frontend/src/api/FileApi.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// Dynamic API URL for local or deployed environments
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://dev-aut-events-induction.vercel.app/api' // production website
+  ? 'https://dev-aut-events-induction.vercel.app/api' // Development website
   : 'http://localhost:8000/api'; // Local development
 
 export const getSignedUrl = async (user, fileName) => {
