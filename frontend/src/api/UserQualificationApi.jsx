f23473b9a7a17f3a5c3cc1e288142150b2bc124f
Thomas Brears <fcx9443@autuni.ac.nz>
2025-06-06 19:05:40 +1200

    dev access

--- frontend/src/api/UserQualificationApi.jsx
+++ frontend/src/api/UserQualificationApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Dynamic API URL
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://inductions.autevents.co.nz/api' // production website
+  ? 'https://dev-aut-events-induction.vercel.app/api' // Development website
   : 'http://localhost:8000/api'; // Local development
 
 export const uploadUserQualification = async (user, file, qualificationData) => {
